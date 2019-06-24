2bb02ac397e62437fdff899e559eb71af3c62751
sairamkotha <sairam.kotha@swiggy.in>
2019-06-24 14:51:25 +0530

    add project file

---
+++ BazelBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BazelBuild
+//
+//  Created by sairam kotha on 24/06/19.
+//  Copyright © 2019 sairam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
